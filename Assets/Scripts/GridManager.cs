f6ad6b4d0ba9efdcdfe60a1eb59b8728bcfe2bdc
FurqaanAziz <147251823+FurqaanAziz@users.noreply.github.com>
2025-09-10 12:49:44 +0400

    -Upgraded Grid Manager
    -Upgraded grid manager
    -Now working for multiple cards
    -Number of cards are equal for match when spawning

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -8,11 +8,14 @@ namespace CardGame
 {
     public class GridManager : MonoBehaviour
     {
-        public GameObject cardPrefab;
+        public List<GameObject> cardPrefabs;
         public int rows = 2;
         public int columns = 2;
         public float spacing = 10f;
         public float aspectRatio = 1.0f;
+
+
+        private Dictionary<string, int> prefabIDs = new Dictionary<string, int>();
 
         void Start()
         {
@@ -22,6 +25,10 @@ namespace CardGame
        
         public void CreateGrid(int rows, int columns)
         {
+            foreach (Transform child in transform)
+            {
+                Destroy(child.gameObject);
+            }
 
             foreach (Transform child in transform)
             {
@@ -29,10 +36,47 @@ namespace CardGame
             }
 
             int totalCards = rows * columns;
+            int totalPairs = totalCards / 2;
 
+            List<int> availableCardIDs = new List<int>();
+
+            List<GameObject> shuffledCardPrefabs = cardPrefabs.OrderBy(x => Guid.NewGuid()).ToList();
+            foreach (var prefab in shuffledCardPrefabs)
+            {
+                if (!prefabIDs.ContainsKey(prefab.name))
+                {
+                    prefabIDs[prefab.name] = prefabIDs.Count;
+                }
+
+                int id = prefabIDs[prefab.name];
+
+                availableCardIDs.Add(id);
+                availableCardIDs.Add(id);
+
+                if (availableCardIDs.Count >= totalCards) break;
+            }
+
+            while (availableCardIDs.Count < totalCards)
+            {
+                foreach (var prefab in shuffledCardPrefabs)
+                {
+                    int id = prefabIDs[prefab.name];
+                    availableCardIDs.Add(id);
+                    availableCardIDs.Add(id);
+                    if (availableCardIDs.Count >= totalCards) break;
+                }
+            }
+            Shuffle(availableCardIDs);
             for (int i = 0; i < totalCards; i++)
             {
-                    GameObject cardInstance = Instantiate(cardPrefab, transform);
+                int cardId = availableCardIDs[i];
+
+                string prefabName = prefabIDs.FirstOrDefault(x => x.Value == cardId).Key;
+                if (prefabName != null)
+                {
+                    GameObject cardInstance = Instantiate(cardPrefabs.Find(p => p.name == prefabName), transform);
+                    Card card = cardInstance.GetComponent<Card>();
+                }
             }
             LayoutRebuilder.ForceRebuildLayoutImmediate(GetComponent<RectTransform>());
         }
@@ -67,6 +111,17 @@ namespace CardGame
             gridLayoutGroup.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
             gridLayoutGroup.constraintCount = columns;
         }
-
+        private void Shuffle(List<int> list)
+        {
+            System.Random rand = new System.Random();
+            int n = list.Count;
+            while (n > 1)
+            {
+                int k = rand.Next(n--);
+                int temp = list[n];
+                list[n] = list[k];
+                list[k] = temp;
+            }
+        }
     }
 }
